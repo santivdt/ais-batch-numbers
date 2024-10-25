a5c065b612e05a7be11e51f8a88cdd0954e2f66f
Santi <santi.vdt@gmail.com>
2024-10-25 08:30:13 +0200

    Check

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Combobox from '@/components/combobox'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-start justify-start p-12'>
+    <main className='p-12'>
       <Combobox />
     </main>
   )
--- components/combobox.tsx
+++ components/combobox.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { ChevronsUpDown } from 'lucide-react'
-import { Batch, batches } from '@/lib/batch'
 import { Button } from '@/components/ui/button'
 import {
   Command,
@@ -16,12 +14,14 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
-import { useState } from 'react'
+import { Batch, batches } from '@/lib/batch'
+import { ChevronsUpDown, Search } from 'lucide-react'
+import { useEffect, useState } from 'react'
 import InfoCard from './info-card'
 
 export function Combobox() {
   const [open, setOpen] = useState(false)
-  const [value, setValue] = useState('')
+  const [showFaq, setShowFaq] = useState(false)
   const [inputValue, setInputValue] = useState('')
   const [currentItem, setCurrentItem] = useState<Batch | null>(null)
 
@@ -29,58 +29,83 @@ export function Combobox() {
     batch.batch_number.includes(inputValue)
   )
 
+  useEffect(() => {
+    setInputValue(batches[0].batch_number)
+    setCurrentItem(batches[0])
+  }, [])
+
   const handleClick = (batch: Batch) => {
-    console.log('clicked')
-    setValue(batch.batch_number)
+    setInputValue(batch.batch_number)
     setCurrentItem(batch)
     setOpen(false)
   }
 
   return (
-    <>
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant='outline'
-            role='combobox'
-            aria-expanded={open}
-            className='w-[500px] justify-between'
-          >
-            {value
-              ? batches.find((batch) => batch.batch_number === value)
-                  ?.batch_number
-              : 'Type your batch number...'}
-            <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className='w-[500px] p-0'>
-          <Command>
-            <CommandInput
-              placeholder='Search batch number...'
-              value={inputValue}
-              onValueChange={setInputValue}
-            />
-            <CommandEmpty>No batch found.</CommandEmpty>
-            <CommandList>
-              <CommandGroup>
-                {filteredBatches.map((batch) => (
-                  <CommandItem
-                    key={batch.batch_number}
-                    value={batch.batch_number}
-                    onSelect={() => handleClick(batch)}
-                  >
-                    {batch.batch_number} - {batch.product} -{' '}
-                    {batch.fishery_name}
-                  </CommandItem>
-                ))}
-              </CommandGroup>
-            </CommandList>
-          </Command>
-        </PopoverContent>
-      </Popover>
-
-      <InfoCard batch={currentItem} />
-    </>
+    <div className='flex gap-8 flex-col md:flex-row'>
+      <div className='flex flex-col gap-4 flex-1'>
+        <p className='text-white font-bold text-2xl'>
+          Type your batch # and find out where your fish was caught:
+        </p>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant='outline'
+              role='combobox'
+              aria-expanded={open}
+              className='justify-between'
+            >
+              <span className='text-rose-600 font-bold flex items-center gap-2'>
+                <Search className='w-4 h-4 text-black' />
+                {inputValue
+                  ? batches.find((batch) => batch.batch_number === inputValue)
+                      ?.batch_number
+                  : ' Search batch number...'}
+              </span>
+              <ChevronsUpDown className='ml-2 h-4 w-4 shrink-0 opacity-50' />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className='p-0'>
+            <Command>
+              <CommandInput
+                placeholder='Search batch number...'
+                value={inputValue}
+                onValueChange={setInputValue}
+              />
+              <CommandEmpty>No batch found.</CommandEmpty>
+              <CommandList>
+                <CommandGroup>
+                  {filteredBatches.map((batch) => (
+                    <CommandItem
+                      key={batch.batch_number}
+                      value={batch.batch_number}
+                      onSelect={() => handleClick(batch)}
+                    >
+                      {batch.batch_number} - {batch.product} -{' '}
+                      {batch.fishery_name}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
+        <h2
+          className='text-yellow-400 font-bold text-lg cursor-pointer'
+          onClick={() => setShowFaq(!showFaq)}
+        >
+          Where is my batch number located?
+        </h2>
+        {showFaq && (
+          <p className='text-white'>
+            Look for the batch number next to the best before date on the back
+            of your product.
+          </p>
+        )}
+      </div>
+      <div className='flex-1'>
+        <InfoCard batch={currentItem} />
+      </div>
+    </div>
   )
 }
 
