a82d2c0ff392e4de80d8fd8648595954c4b04b06
Santi <santi.vdt@gmail.com>
2024-11-16 10:08:05 +0100

    Make mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Combobox from '@/components/combobox'
 
 export default function Home() {
   return (
-    <main className='p-12'>
+    <main className='p-0 md:p-12'>
       <Combobox />
     </main>
   )
